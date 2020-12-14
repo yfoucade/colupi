d81bbf3268109da137d85160355fa1d910b6ab50
Yohan Foucade <foucade@lipn.fr>
2020-12-14 23:07:32 +0100

    Add uspn_ugtm package

---
+++ src/models/uspn_ugtm/__init__.py
@@ -0,0 +1,27 @@
+"""ugtm: a python package for Generative Topographic Mapping (GTM)
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+    ugtm_sklearn
+    ugtm_gtm
+    ugtm_kgtm
+    ugtm_classes
+    ugtm_plot
+    ugtm_landscape
+    ugtm_predictions
+    ugtm_crossvalidate
+    ugtm_preprocess
+"""
+
+from .ugtm_plot import *
+from .ugtm_landscape import *
+from .ugtm_gtm import *
+from .ugtm_kgtm import *
+from .ugtm_predictions import *
+from .ugtm_crossvalidate import *
+from .ugtm_preprocess import *
+from .ugtm_sklearn import *
